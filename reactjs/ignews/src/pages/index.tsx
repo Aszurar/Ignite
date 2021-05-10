63e1f3cdaa51fc1f752d2d6d85860d323eaeaf84
Lucas de Lima <lms.souza39@gmail.com>
2021-05-10 18:57:25 -0300

    :tada: api call maked :tada:

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import {SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/strype';
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   // chamada a API de pagamentos STRIPE pegando todas informações de nosso produto incluíndo a que utilizaremos que é 
   // seu valor
@@ -55,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 //24 hours
   }
 }
