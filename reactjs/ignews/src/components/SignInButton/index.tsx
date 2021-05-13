77d1d9d0ec7346b83b02af7d506d2792e97bc3b8
Lucas de Lima <lms.souza39@gmail.com>
2021-05-13 18:22:43 -0300

    :tada: authenticator complete with next-auth :tada:

--- reactjs/ignews/src/components/SignInButton/index.tsx
+++ reactjs/ignews/src/components/SignInButton/index.tsx
@@ -1,12 +1,15 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
-
+import { signIn, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+    const [ session ] = useSession()
 
-    return isUserLoggedIn ? (
+    console.log(session);
+    
+        
+    return session ? (
         <button type="button"
             className={styles.signInButton}
         >
@@ -17,6 +20,7 @@ export function SignInButton() {
     ) : (
         <button type="button"
                 className={styles.signInButton}
+                onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign in with GitHub
---
+++ reactjs/ignews/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: 'read:user'
+    }),
+    // ...add more providers here
+  ],
+})
