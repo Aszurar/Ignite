db91f96f3a81bcd364cba49bdf8cb7f69efce5ef
Lucas de Lima <lms.souza39@gmail.com>
2021-05-14 11:46:19 -0300

    :tada: authenticator complete and faunaDB settings stated :tada:

--- reactjs/ignews/src/components/SignInButton/index.tsx
+++ reactjs/ignews/src/components/SignInButton/index.tsx
@@ -1,20 +1,19 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
-import { signIn, useSession } from 'next-auth/client';
+import { signIn, signOut,useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 export function SignInButton() {
     const [ session ] = useSession()
 
-    console.log(session);
-    
-        
     return session ? (
         <button type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361" />
-            Lucas de Lima
+            <img src={session.user.image} alt="Profile Image"/>
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
