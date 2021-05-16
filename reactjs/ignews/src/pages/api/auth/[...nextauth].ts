0434bd787aff1f6bace584fc4ac5bbdf2aca962b
Lucas de Lima <lms.souza39@gmail.com>
2021-05-16 18:33:44 -0300

    :tada: faunadb setted :tada:

--- reactjs/ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,4 +14,38 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+
+  callbacks: {
+    async signIn(user, account, profile) {
+      // console.log(user); test!
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match( //procurando por um usuário pelo e-mail e comparando o e-mail do usuário
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+        q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 })
