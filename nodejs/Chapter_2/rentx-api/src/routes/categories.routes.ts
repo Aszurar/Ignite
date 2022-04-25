4e73cc225b789cb26610db441e0ab4420ad8091a
Lucas de Lima <lms.souza39@gmail.com.br>
2022-04-25 00:18:44 -0300

    :tada: documentation and upload of archives complete :tada:

--- nodejs/Chapter_2/rentx-api/src/routes/categories.routes.ts
+++ nodejs/Chapter_2/rentx-api/src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoriesController } from "../modules/cars/useCases/ImportCategories";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+const upload = multer({
+    dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req: Request, res: Response) => {
     return createCategoryController.handle(req, res);
@@ -12,5 +17,13 @@ categoriesRoutes.post("/", (req: Request, res: Response) => {
 categoriesRoutes.get("/", (req: Request, res: Response) => {
     return listCategoriesController.handle(req, res);
 });
+
+categoriesRoutes.post(
+    "/imports",
+    upload.single("file"),
+    (req: Request, res: Response) => {
+        return importCategoriesController.handle(req, res);
+    }
+);
 
 export { categoriesRoutes };
--- nodejs/Chapter_2/rentx-api/src/server.ts
+++ nodejs/Chapter_2/rentx-api/src/server.ts
@@ -1,16 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json({ message: "Hello World" });
-});
-
 app.use(router);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => {
     console.log("Server is running on port 3000");
