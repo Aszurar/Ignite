6a66678e930e971dacd851bbe3d64379a197bf16
Lucas de Lima <lms.souza39@gmail.com.br>
2022-11-02 19:47:55 -0300

    :heavy_plus_sign: :art: Instalação das bibliotecas Styled-Components e babel plugin module resolve e configuração de Path Mapping :art: :heavy_plus_sign:

--- react_native/Chapter_2/igniteteams/babel.config.js
+++ react_native/Chapter_2/igniteteams/babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@assets': './assets',
+            '@components': './src/components',
+            '@hooks': './src/hooks',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@theme': './src/theme',
+            '@types': './src/types',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
