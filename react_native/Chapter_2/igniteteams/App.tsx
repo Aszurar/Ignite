ab283d9a3416606cfd3cf1300d7dcff888aa6499
Lucas de Lima <lms.souza39@gmail.com.br>
2022-11-02 19:52:50 -0300

    :sparkles: Uso das fontes da aplicação e do tema global :sparkles:

--- react_native/Chapter_2/igniteteams/App.tsx
+++ react_native/Chapter_2/igniteteams/App.tsx
@@ -1,7 +1,19 @@
-import { Groups } from './src/screens/Groups';
+import { ThemeProvider } from 'styled-components/native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
+import theme from '@theme/index';
+import { Loading } from '@components/Loading';
+import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      {
+        fontsLoaded ? <Groups /> : <Loading />
+      }
+    </ThemeProvider>
   );
 }
