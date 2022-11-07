5509cf8911b509da173726ae675f7bd52da98424
Lucas de Lima <lms.souza39@gmail.com.br>
2022-11-07 12:55:13 -0300

    ✨Projeto IgniteTeams: Criação da tela de Grupos✨

--- react_native/Chapter_2/igniteteams/src/screens/Groups/index.tsx
+++ react_native/Chapter_2/igniteteams/src/screens/Groups/index.tsx
@@ -1,10 +1,56 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Header } from '@components/Header';
+import { Container, GroupList, Separator, SubHeader } from './styles';
+import { HighlightText } from '@components/HighlightText';
+import { GroupCard } from '@components/GroupCard';
+import { View } from 'react-native';
+
+export type GroupProps = {
+  id: string,
+  name: string,
+  image: string,
+  amount: string,
+  description: string,
+}
+
+const DATA = {
+  id: "1",
+  name: "Grupo de estudos",
+  image: "https://avatars.githubusercontent.com/u/60210220?v=4",
+  amount: "10",
+  description: "Grupo de estudos de React Native",
+}
+
+const DATA2 = {
+  id: "2",
+  name: "Grupo de  jogos",
+  image: "https://cdn2.steamgriddb.com/file/sgdb-cdn/icon_thumb/b21f9f98829dea9a48fd8aaddc1f159d.png",
+  amount: "32",
+  description: "Grupo de jogos de RPG",
+}
 
 export function Groups() {
+  const [groups, setGroups] = useState<GroupProps[]>([DATA, DATA2])
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Groups</Text>
-    </View>
+    <Container >
+      <Header />
+      <SubHeader>
+        <HighlightText title="Turmas" subtitle="jogue com a sua turma" />
+      </SubHeader>
+      <GroupList
+        data={groups}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item.name}
+            subtitle={item.amount}
+            image={item.image}
+            onPress={() => console.log("Yes")}
+          />
+        )}
+        ItemSeparatorComponent={() => <Separator />}
+      />
+    </Container>
   );
 }
---
+++ react_native/Chapter_2/igniteteams/src/screens/Groups/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+import { FlatList, FlatListProps } from 'react-native';
+import { GroupProps } from '.';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+`;
+
+export const SubHeader = styled.View`
+  margin: 32px 0;
+`;
+
+export const GroupList = styled(FlatList as new (props: FlatListProps<GroupProps>) => FlatList<GroupProps>).attrs({
+  showsVerticalScrollIndicator: false,
+})``;
+
+export const Separator = styled.View`
+  height: 0.5px;
+  width: 10%;
+  align-self: center;
+  background-color: ${({ theme }) => theme.COLORS.GREEN_700};
+
+  margin: 12px 0;
+`;
